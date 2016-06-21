7ae9bfc62f539dbfcd3a5f6be2a680c1002c2ed4
Tyler Wong <tbwong3@gmail.com>
2016-06-20 20:06:52 -0700

    Initial commit

---
+++ app/src/main/java/com/example/tylerbwong/awaken/activities/SplashActivity.java
@@ -0,0 +1,9 @@
+package com.example.tylerbwong.awaken.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Tyler Wong
+ */
+public class SplashActivity extends AppCompatActivity {
+}
