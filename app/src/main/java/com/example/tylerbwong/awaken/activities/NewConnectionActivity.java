fcfd9e8c2784f89c43f8043a26414f13e5eec900
cdubthecoolcat <cewong12297@gmail.com>
2017-06-29 00:33:36 -0700

    fixed ui lag

--- app/src/main/java/com/example/tylerbwong/awaken/activities/NewConnectionActivity.java
+++ app/src/main/java/com/example/tylerbwong/awaken/activities/NewConnectionActivity.java
@@ -1,8 +1,10 @@
 package com.example.tylerbwong.awaken.activities;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -20,146 +22,157 @@ import com.example.tylerbwong.awaken.network.StatusUpdate;
  */
 public class NewConnectionActivity extends AppCompatActivity {
 
-   private TextInputEditText mNicknameInput;
-   private TextInputEditText mHostInput;
-   private TextInputEditText mMacInput;
-   private TextInputEditText mWolPortInput;
-   private TextInputEditText mDevicePortInput;
-   private Button mEnterButton;
+    private TextInputEditText mNicknameInput;
+    private TextInputEditText mHostInput;
+    private TextInputEditText mMacInput;
+    private TextInputEditText mWolPortInput;
+    private TextInputEditText mDevicePortInput;
+    private Button mEnterButton;
 
-   private ConnectionDatabaseHelper mDatabaseHelper;
-   private String mNickname;
-   private String mHost;
-   private String mMac;
-   private String mPortWol;
-   private String mDevicePort;
-   private String mCity;
-   private String mState;
-   private String mCountry;
-   private String mStatus;
-   private boolean mHasTextHost = false;
-   private boolean mHasTextPortWol = false;
+    private ConnectionDatabaseHelper mDatabaseHelper;
+    private String mNickname;
+    private String mHost;
+    private String mMac;
+    private String mPortWol;
+    private String mDevicePort;
+    private String mCity;
+    private String mState;
+    private String mCountry;
+    private boolean mHasTextHost = false;
+    private boolean mHasTextPortWol = false;
 
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_new_connection);
-      mDatabaseHelper = new ConnectionDatabaseHelper(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_connection);
+        mDatabaseHelper = new ConnectionDatabaseHelper(this);
 
-      mNicknameInput = (TextInputEditText) findViewById(R.id.nickname_input);
-      mHostInput = (TextInputEditText) findViewById(R.id.host_input);
-      mMacInput = (TextInputEditText) findViewById(R.id.mac_input);
-      mWolPortInput = (TextInputEditText) findViewById(R.id.wol_input);
-      mDevicePortInput = (TextInputEditText) findViewById(R.id.port_input);
-      mEnterButton = (Button) findViewById(R.id.enter_button);
+        mNicknameInput = (TextInputEditText) findViewById(R.id.nickname_input);
+        mHostInput = (TextInputEditText) findViewById(R.id.host_input);
+        mMacInput = (TextInputEditText) findViewById(R.id.mac_input);
+        mWolPortInput = (TextInputEditText) findViewById(R.id.wol_input);
+        mDevicePortInput = (TextInputEditText) findViewById(R.id.port_input);
+        mEnterButton = (Button) findViewById(R.id.enter_button);
 
-      mEnterButton.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View view) {
-            enterAction();
-         }
-      });
-
-      mHostInput.addTextChangedListener(new TextWatcher() {
-
-         @Override
-         public void onTextChanged(CharSequence sequence, int start, int before, int count) {
-            if (sequence.toString().trim().length() == 0) {
-               mHasTextHost = false;
+        mEnterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enterAction();
             }
-            else {
-               mHasTextHost = true;
+        });
+
+        mHostInput.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence sequence, int start, int before, int count) {
+                if (sequence.toString().trim().length() == 0) {
+                    mHasTextHost = false;
+                } else {
+                    mHasTextHost = true;
+                }
+                checkFields();
             }
-            checkFields();
-         }
 
-         @Override
-         public void beforeTextChanged(CharSequence sequence, int start, int count, int after) {
+            @Override
+            public void beforeTextChanged(CharSequence sequence, int start, int count, int after) {
 
-         }
-
-         @Override
-         public void afterTextChanged(Editable editable) {
-         }
-      });
-
-      mWolPortInput.addTextChangedListener(new TextWatcher() {
-
-         @Override
-         public void onTextChanged(CharSequence sequence, int start, int before, int count) {
-            if (sequence.toString().trim().length() == 0) {
-               mHasTextPortWol = false;
             }
-            else {
-               mHasTextPortWol = true;
+
+            @Override
+            public void afterTextChanged(Editable editable) {
             }
-            checkFields();
-         }
+        });
 
-         @Override
-         public void beforeTextChanged(CharSequence sequence, int start, int count, int after) {
+        mWolPortInput.addTextChangedListener(new TextWatcher() {
 
-         }
+            @Override
+            public void onTextChanged(CharSequence sequence, int start, int before, int count) {
+                if (sequence.toString().trim().length() == 0) {
+                    mHasTextPortWol = false;
+                } else {
+                    mHasTextPortWol = true;
+                }
+                checkFields();
+            }
 
-         @Override
-         public void afterTextChanged(Editable editable) {
-         }
-      });
+            @Override
+            public void beforeTextChanged(CharSequence sequence, int start, int count, int after) {
 
-      mEnterButton.setEnabled(false);
-   }
+            }
 
-   private void checkFields() {
-      if (mHasTextHost && mHasTextPortWol) {
-         mEnterButton.setEnabled(true);
-      }
-      else {
-         mEnterButton.setEnabled(false);
-      }
-   }
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
 
-   private void switchToMain() {
-      Intent mainIntent = new Intent(NewConnectionActivity.this, MainActivity.class);
-      startActivity(mainIntent);
-      finish();
-   }
+        mEnterButton.setEnabled(false);
+    }
 
-   private void enterAction() {
-      mNickname = mNicknameInput.getText().toString();
-      mHost = mHostInput.getText().toString();
-      mMac = formatMac(mMacInput.getText().toString());
-      mPortWol = mWolPortInput.getText().toString();
-      mDevicePort = mDevicePortInput.getText().toString();
-      Location newLocation = new Location(mHost);
-      mCity = newLocation.getCity();
-      mState = newLocation.getStateProv();
-      mCountry = newLocation.getCountryName();
-      mStatus = String.valueOf(StatusUpdate.getStatus(mHost, Integer.parseInt(mDevicePort)));
+    private void checkFields() {
+        if (mHasTextHost && mHasTextPortWol) {
+            mEnterButton.setEnabled(true);
+        } else {
+            mEnterButton.setEnabled(false);
+        }
+    }
 
-      String message;
-      try {
-         mDatabaseHelper.insertConnection(mNickname, mHost, mMac, mPortWol,
-               mDevicePort, mCity, mState, mCountry, mStatus, "");
-         message = getResources().getString(R.string.new_connection_success);
-         switchToMain();
-      }
-      catch (Exception e) {
-         message = getResources().getString(R.string.new_connection_fail);
-      }
-      Toast.makeText(NewConnectionActivity.this, message, Toast.LENGTH_LONG).show();
-   }
+    private void switchToMain() {
+        Intent mainIntent = new Intent(NewConnectionActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
 
-   private static String formatMac(String mac) {
-      String formattedMac = "";
-      mac = mac.replace("-", "");
-      mac = mac.replace(":", "");
-      for (int i = 0; i < mac.length(); i += 2) {
-         formattedMac += mac.substring(i, i + 2);
-         if (i < mac.length() - 2) {
-            formattedMac += ":";
-         }
-      }
-      formattedMac = formattedMac.toUpperCase();
-      return formattedMac;
-   }
+    private void enterAction() {
+        new AsyncTaskCaller().execute();
+        switchToMain();
+    }
+
+    private class AsyncTaskCaller extends AsyncTask<Void, Void, Pair<String, Location>> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mNickname = mNicknameInput.getText().toString();
+            mHost = mHostInput.getText().toString();
+            mMac = formatMac(mMacInput.getText().toString());
+            mPortWol = mWolPortInput.getText().toString();
+            mDevicePort = mDevicePortInput.getText().toString();
+        }
+        @Override
+        protected Pair<String, Location> doInBackground(Void... pars) {
+            return new Pair<>(String.valueOf(StatusUpdate.getStatus(mHost,
+                    Integer.parseInt(mDevicePort))), new Location(mHost));
+        }
+        @Override
+        protected void onPostExecute(Pair<String, Location> statLoc) {
+            super.onPostExecute(statLoc);
+            Location newLocation = statLoc.second;
+            mCity = newLocation.getCity();
+            mState = newLocation.getStateProv();
+            mCountry = newLocation.getCountryName();
+            String message;
+            try {
+                mDatabaseHelper.insertConnection(mNickname, mHost, mMac, mPortWol,
+                        mDevicePort, mCity, mState, mCountry, statLoc.first, "");
+                message = getResources().getString(R.string.new_connection_success);
+
+            } catch (Exception e) {
+                message = getResources().getString(R.string.new_connection_fail);
+            }
+            Toast.makeText(NewConnectionActivity.this, message, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private static String formatMac(String mac) {
+        String formattedMac = "";
+        mac = mac.replace("-", "");
+        mac = mac.replace(":", "");
+        for (int i = 0; i < mac.length(); i += 2) {
+            formattedMac += mac.substring(i, i + 2);
+            if (i < mac.length() - 2) {
+                formattedMac += ":";
+            }
+        }
+        formattedMac = formattedMac.toUpperCase();
+        return formattedMac;
+    }
 }
