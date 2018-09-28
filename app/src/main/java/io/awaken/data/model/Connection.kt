4d99786de8e5f426c1db0e1553809c136e13ff05
Tyler Wong <tbwong3@gmail.com>
2018-09-27 19:48:55 -0700

    Convert StatusUpdate to Kotlin

--- app/src/main/java/io/awaken/data/model/Connection.kt
+++ app/src/main/java/io/awaken/data/model/Connection.kt
@@ -1,6 +1,6 @@
 package io.awaken.data.model
 
-import io.awaken.data.network.StatusUpdate
+import io.awaken.data.network.isRunning
 import io.reactivex.Single
 
 /**
@@ -8,5 +8,5 @@ import io.reactivex.Single
  */
 class Connection(val id: Int, val nickname: String, val host: String, val mac: String, val portWol: String, val portDev: String,
                       val city: String, val state: String, val country: String, val status: String, val date: String) {
-    fun isRunning(): Single<Boolean> = StatusUpdate.isRunning(host, Integer.parseInt(portDev))
+    fun isRunning(): Single<Boolean> = isRunning(host, Integer.parseInt(portDev))
 }
---
+++ app/src/main/java/io/awaken/data/network/StatusUpdate.kt
@@ -0,0 +1,40 @@
+package io.awaken.data.network
+
+import io.reactivex.Single
+import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Socket
+
+/**
+ * How long the socket should wait for a response
+ * before stopping.
+ */
+private const val TIMEOUT = 500
+
+/**
+ * Gets the current status of the device in question. The method
+ * will attempt to establish a socket connection with the target
+ * device. If no connection can be made, the device is considered
+ * INACTIVE.
+ *
+ * @param host       - the name of the host/ip address
+ * @param devicePort - the port that the router forwards to your device
+ * @return true if the current status of the machine is RUNNING or false if the current status is
+ * INACTIVE
+ */
+fun isRunning(host: String, devicePort: Int): Single<Boolean> {
+    return Single.fromCallable {
+        var status = true
+        try {
+            val address = InetSocketAddress(host, devicePort)
+            val statusSocket = Socket()
+            statusSocket.connect(address, TIMEOUT)
+            statusSocket.close()
+        }
+        catch (e: IOException) {
+            status = false
+        }
+
+        status
+    }
+}
