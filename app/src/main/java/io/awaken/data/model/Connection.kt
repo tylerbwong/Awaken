0eb08e08fcd24f9d8568364eaaef3e1851d98ce7
Tyler Wong <tbwong3@gmail.com>
2018-09-27 19:41:06 -0700

    format

--- app/src/main/java/io/awaken/data/model/Connection.kt
+++ app/src/main/java/io/awaken/data/model/Connection.kt
@@ -8,7 +8,5 @@ import io.reactivex.Single
  */
 class Connection(val id: Int, val nickname: String, val host: String, val mac: String, val portWol: String, val portDev: String,
                       val city: String, val state: String, val country: String, val status: String, val date: String) {
-    fun isRunning(): Single<Boolean> {
-        return StatusUpdate.isRunning(host, Integer.parseInt(portDev));
-    }
+    fun isRunning(): Single<Boolean> = StatusUpdate.isRunning(host, Integer.parseInt(portDev))
 }
