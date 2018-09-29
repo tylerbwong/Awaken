8ed99f469abfde7a6cdae974325e889a084dff76
Tyler Wong <tbwong3@gmail.com>
2018-09-29 01:38:25 -0700

    Update ConnectionsFragment.kt

--- app/src/main/java/io/awaken/ui/connections/ConnectionsFragment.kt
+++ app/src/main/java/io/awaken/ui/connections/ConnectionsFragment.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
