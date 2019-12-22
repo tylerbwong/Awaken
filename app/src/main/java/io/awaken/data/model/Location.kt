605e563cd73743a4515e254a132cdf92741c90ba
Connor <cdubthecoolcat@gmail.com>
2019-12-22 14:44:29 -0800

    remove unused code

--- app/src/main/java/io/awaken/data/model/Location.kt
+++ app/src/main/java/io/awaken/data/model/Location.kt
@@ -10,14 +10,6 @@ class Location {
     val city: String? = null
     @SerializedName("region_code")
     val regionCode: String? = null
-    @SerializedName("country_code")
-    val countryCode: String? = null
-    @SerializedName("ip")
-    val ip: String? = null
     @SerializedName("country_name")
     val countryName: String? = null
-    @SerializedName("continent_name")
-    val continentName: String? = null
-    @SerializedName("continent_code")
-    val continentCode: String? = null
 }
